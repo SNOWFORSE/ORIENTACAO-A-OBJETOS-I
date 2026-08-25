cb12a2e7d0aeed4f164ea372193439e573d3c3ca
Gustavo H. Apolinário <134400943+SNOWFORSE@users.noreply.github.com>
2026-08-24 21:49:29 -0300

    Create exer1.java with main method and input setup
    Initialize arrays for integer and string input.

---
+++ JAVA/exer1.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class exer1 {
+    public static void main(String[] args)  throws Exception{
+        BufferedReader reader = new BufferedReader(
+            new InputStreamReader(System.in)
+        );
+
+        int tamanhoVetor = 4;
+        int vetA[] = new int {tamanhoVetor};
+        String vetB[] = new String {tamanhoVetor}
+        System.out.println("Digite um valor" + vetA);
+
+        
+    }
+}

5b2330fe95c971af494b3c1c86a6319513505559
Gustavo H. Apolinário <134400943+SNOWFORSE@users.noreply.github.com>
2026-09-14 21:47:17 -0300

    Create Companhia.java

---
+++ Vetor de Voo/Companhia.java
@@ -0,0 +1,11 @@
+public class Companhia {
+    private String NumVoo;
+    private float QtdVoo;
+
+    public String NumVoo(){
+        return this.NumVoo;
+    }
+    public float QtdVoo(){
+        return this.QtdVoo;
+    }
+}
